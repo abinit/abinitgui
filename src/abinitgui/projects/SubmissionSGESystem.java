496b817b3d29e47b7e00233d0f5cacf61181770f
yanikou19 <yanikou19@gmail.com>
2015-01-22 20:42:08 +0000

    Implementing some status stuff for SGE... Does not work as well as SLURM since the output of qstat and qacct is not as clear as scontrol and sacct !!!
    git-svn-id: https://subversion.assembla.com/svn/abinitgui/AbinitGUI-new-git@279 2eda1b6a-27ca-4413-aee3-c8a7c7939794

--- src/abinitgui/projects/SubmissionSGESystem.java
+++ src/abinitgui/projects/SubmissionSGESystem.java
@@ -8,7 +8,14 @@ package abinitgui.projects;
 import abinitgui.core.MainFrame;
 import abinitgui.core.RetMSG;
 import abinitgui.core.Utils;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 
 /**
  *
@@ -76,12 +83,214 @@ public class SubmissionSGESystem extends SubmissionSystem
 
     @Override
     public ArrayList<RemoteJob> getRemoteJobs() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        ArrayList<RemoteJob> listJobs = new ArrayList<>();
+        
+        
+        if(!machine.isConnected())
+            machine.connection();
+        
+        if(!machine.isConnected())
+        {
+            MainFrame.printERR("Cannot connect to the machine !");
+            return null;
+        }
+        
+        String login = null;
+        if(machine.getType() == Machine.REMOTE_MACHINE || machine.getType() == Machine.GATEWAY_MACHINE)
+        {
+            login = machine.getRemoteConnect().getLogin();
+        }
+        if(login == null)
+        {
+            MainFrame.printERR("Cannot get login for the machine !");
+            return null;
+        }
+        // Zombie jobs
+        RetMSG msg = machine.sendCommand("qstat -u "+login+" -s z -xml",false);
+        String retMsg = msg.getRetMSG();
+        SAXBuilder sxb = new SAXBuilder();
+        try {
+            Document document = sxb.build(new StringReader(retMsg));
+            Element root = document.getRootElement();
+            System.out.println(root);
+            Element root2 = root.getChild("queue_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                RemoteJob rj = new RemoteJob();
+                rj.setMachineName(machine.getName());
+                rj.setJobId(Integer.parseInt(jIS));
+                rj.setStatusString("Unknown");
+                listJobs.add(rj);
+            }
+            root2 = root.getChild("job_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                RemoteJob rj = new RemoteJob();
+                rj.setMachineName(machine.getName());
+                rj.setJobId(Integer.parseInt(jIS));
+                rj.setStatusString("Unknown");
+                listJobs.add(rj);
+            }
+            
+        } catch (JDOMException | IOException e) {
+            MainFrame.printERR(e.getMessage());
+        }
+        
+        msg = machine.sendCommand("qstat -u "+login+" -s r -xml",false);
+        retMsg = msg.getRetMSG();
+        System.out.println(retMsg);
+        try {
+            Document document = sxb.build(new StringReader(retMsg));
+            Element root = document.getRootElement();
+            Element root2 = root.getChild("job_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                RemoteJob rj = new RemoteJob();
+                rj.setMachineName(machine.getName());
+                rj.setJobId(Integer.parseInt(jIS));
+                rj.setStatusString("Running");
+                listJobs.add(rj);
+            }
+            root2 = root.getChild("queue_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                RemoteJob rj = new RemoteJob();
+                rj.setMachineName(machine.getName());
+                rj.setJobId(Integer.parseInt(jIS));
+                rj.setStatusString("Running");
+                listJobs.add(rj);
+            }
+            
+        } catch (JDOMException | IOException e) {
+            MainFrame.printERR(e.getMessage());
+        }
+        
+        
+        
+        msg = machine.sendCommand("qstat -u "+login+" -s p -xml",false);
+        retMsg = msg.getRetMSG();
+        try {
+            Document document = sxb.build(new StringReader(retMsg));
+            Element root = document.getRootElement();
+            System.out.println(root);
+            Element root2 = root.getChild("job_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                RemoteJob rj = new RemoteJob();
+                rj.setMachineName(machine.getName());
+                rj.setJobId(Integer.parseInt(jIS));
+                rj.setStatusString("Pending");
+                listJobs.add(rj);
+            } 
+            root2 = root.getChild("queue_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                RemoteJob rj = new RemoteJob();
+                rj.setMachineName(machine.getName());
+                rj.setJobId(Integer.parseInt(jIS));
+                rj.setStatusString("Pending");
+                listJobs.add(rj);
+            }
+            
+        } catch (JDOMException | IOException e) {
+            MainFrame.printERR(e.getMessage());
+        }
+        
+        return listJobs;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void updateStatus(RemoteJob rj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         if(!machine.isConnected())
+            machine.connection();
+        
+        if(!machine.isConnected())
+        {
+            MainFrame.printERR("Cannot connect to the machine !");
+            return;
+        }
+        
+        String login = null;
+        if(machine.getType() == Machine.REMOTE_MACHINE || machine.getType() == Machine.GATEWAY_MACHINE)
+        {
+            login = machine.getRemoteConnect().getLogin();
+        }
+        if(login == null)
+        {
+            MainFrame.printERR("Cannot get login for the machine !");
+            return;
+        }
+
+        rj.setStatus(RemoteJob.UNKNOWN);
+        RetMSG msg = machine.sendCommand("qstat -u "+login+" -r -xml",false);
+        String retMsg = msg.getRetMSG();
+        SAXBuilder sxb = new SAXBuilder();
+        try {
+            Document document = sxb.build(new StringReader(retMsg));
+            Element root = document.getRootElement();
+            System.out.println(root);
+            Element root2 = root.getChild("queue_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                if(jIS.equals(""+rj.getJobId()))
+                {
+                    String status = value.getChild("state").getValue();
+                    switch(status)
+                    {
+                        case "qw":
+                            rj.setStatus(RemoteJob.PENDING);
+                            break;
+                        case "r":
+                            rj.setStatus(RemoteJob.RUNNING);
+                            break;
+                        case "Eqw":
+                            rj.setStatus(RemoteJob.FAILED);
+                            break;
+                    }
+                }
+            }
+            root2 = root.getChild("job_info");
+            for(Object job : root2.getChildren())
+            {
+                Element value = (Element)job;
+                String jIS = value.getChild("JB_job_number").getValue();
+                if(jIS.equals(""+rj.getJobId()))
+                {
+                    String status = value.getChild("state").getValue();
+                    switch(status)
+                    {
+                        case "qw":
+                            rj.setStatus(RemoteJob.PENDING);
+                            break;
+                        case "r":
+                            rj.setStatus(RemoteJob.RUNNING);
+                            break;
+                        case "Eqw":
+                            rj.setStatus(RemoteJob.FAILED);
+                            break;
+                    }
+                }
+            }
+            
+        } catch (JDOMException | IOException e) {
+            MainFrame.printERR(e.getMessage());
+        }
     }
 
     @Override
@@ -94,6 +303,12 @@ public class SubmissionSGESystem extends SubmissionSystem
             getMachine().connection();
         
         RetMSG msg = getMachine().sendCommand("qacct -j "+rj.getJobId());
+        
+        if(msg.getRetCode() != 0)
+        {
+            msg = getMachine().sendCommand("qstat -j "+rj.getJobId());
+            return msg.getRetMSG();
+        }
         return msg.getRetMSG();
     }
 
