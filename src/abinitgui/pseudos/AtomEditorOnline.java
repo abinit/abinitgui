2a95dd0688b93988b6a1a3b3cf6a94fac4e21bf4
yanikou19 <yanikou19@gmail.com>
2015-02-04 23:18:45 +0000

    For the buttons, we should overwrite the button UI to design our own rectangle buttons for pseudos... I'd to remove the mouse listeners of the buttons otherwise the lists are still there if I click on a disabled button !
    git-svn-id: https://subversion.assembla.com/svn/abinitgui/AbinitGUI-new-git@288 2eda1b6a-27ca-4413-aee3-c8a7c7939794

--- src/abinitgui/pseudos/AtomEditorOnline.java
+++ src/abinitgui/pseudos/AtomEditorOnline.java
@@ -59,6 +59,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -459,6 +460,14 @@ public class AtomEditorOnline extends AbstractCellEditor
         if (button == null) {
             return;
         }
+        
+        
+        MouseListener[] listeners = button.getMouseListeners();
+        for(MouseListener listen : listeners)
+        {
+            button.removeMouseListener(listen);
+        }
+                    
         PseudoDatabase rDatabase = MainFrame.getRemotePseudoDatabase();
         
         ArrayList<AbinitPseudo> listPseudos = rDatabase.getListPseudos(pspType, symbol);
